20dfa5bb2c9e51d609c8ed57cba3f4bd543b5156
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-07 04:46:38 -0700

    Base dependencies installed. package.json setup. weback setup. index.html started. Ready to start writing React components.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"
+      }
+    ]
+  },
+  output: {
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+}
