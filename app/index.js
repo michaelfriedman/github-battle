eddfc33a612783e130974c9b79b37dd6a29c7bf4
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-07 05:12:36 -0700

    First React test module/class is successfully rendering to the page.

--- app/index.js
+++ app/index.js
@@ -1,2 +1,15 @@
-const app = document.getElementById('app')
-app.innerHTML = 'Testing, my name is Michael. I program UI using React.'
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+const HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div> Hello World Program </div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+)
