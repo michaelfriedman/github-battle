09adb172a3f45df80a538aa6262936645f5fa2d5
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-10 10:58:58 -0700

    Fixing formatting using standard guidlines.

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,17 +4,17 @@ const ReactRouter = require('react-router')
 const Link = ReactRouter.Link
 
 const Home = React.createClass({
-    render: function () {
-        return (
+  render: function () {
+    return (
       <div className='jumbotron col-sm-12 text-center' style={transparentBg}>
-      <h1> Github Battle </h1>
-      <p className='lead'>This fancy placeholder</p>
-      <Link to='/playerOne'>
-        <button type='button' className='btn btn-lg btn-success'>Get Started</button>
-      </Link>
+        <h1> Github Battle </h1>
+        <p className='lead'>This fancy placeholder</p>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-success'>Get Started</button>
+        </Link>
       </div>
     )
-    }
+  }
 })
 
 module.exports = Home
