17df7888bb429779cf1f916a0a53149ecdd6558b
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-10 14:18:05 -0700

    Winner and Loser battle calculations successfully rendering to UI.

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,14 +1,25 @@
 const React = require('react')
 const PropTypes = React.PropTypes
+const styles = require('../styles')
+const UserDetails = require('./UserDetails')
+const UserDetailsWrapper = require('./UserDetailsWrapper')
 
-function puke (obj) {
-  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-  console.log('puking')
-}
 
 function Results (props) {
+  const winningIndex = props.scores[0] > props.scores[1] ? 0 : 1
+  const losingIndex = winningIndex === 0 ? 1 : 0
   return (
-    <div> Results </div>
+    <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+      <h1> Results </h1>
+      <div className='sol-sm-8 col-sm-offset-2'>
+        <UserDetailsWrapper header='Winner'>
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header='Loser'>
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+        </UserDetailsWrapper>
+      </div>
+    </div>
   )
 }
 
