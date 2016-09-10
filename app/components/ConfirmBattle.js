c0fa7afe56d497eb6881423ec671a3d05b3cd6de
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-10 10:51:54 -0700

    Fixing formatting using standard guidelines.

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -5,36 +5,36 @@ const Link = require('react-router').Link
 const UserDetailsWrapper = require('./UserDetailsWrapper')
 const UserDetails = require('./UserDetails')
 
-function puke(obj) {
+function puke (obj) {
   return <pre> {JSON.stringify(obj, null, ' ')}</pre>
 }
 
-function ConfirmBattle(props) {
-  return props.isLoading === true ?
-    <p> LOADING </p> :
-    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
-      <h1>Confirm Players</h1>
-      <div className='col-sm-8 col-sm-offset-2'>
-        <UserDetailsWrapper header='Player One'>
-          <UserDetails info={props.playersInfo[0]} />
-          </UserDetailsWrapper>
-          <UserDetailsWrapper header='Player Two'>
-            <UserDetails info={props.playersInfo[1]} />
-            </UserDetailsWrapper>
-          </div>
-          <div className='col-sm-8 col-sm-offset-2'>
-            <div className='col-sm-12' style={styles.space}>
-              <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
-              Initiate battle
-              </button>
-            </div>
-            <div className='col-sm-12' style={styles.space}>
-            <Link to='/playerOne'>
-            <button type='button' className='btn btn-lg btn-danger'>Reselect players</button>
-            </Link>
-            </div>
-          </div>
+function ConfirmBattle (props) {
+  return props.isLoading === true
+  ? <p> LOADING </p>
+  : <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+    <h1>Confirm Players</h1>
+    <div className='col-sm-8 col-sm-offset-2'>
+      <UserDetailsWrapper header='Player One'>
+        <UserDetails info={props.playersInfo[0]} />
+      </UserDetailsWrapper>
+      <UserDetailsWrapper header='Player Two'>
+        <UserDetails info={props.playersInfo[1]} />
+      </UserDetailsWrapper>
+    </div>
+    <div className='col-sm-8 col-sm-offset-2'>
+      <div className='col-sm-12' style={styles.space}>
+        <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
+          Initiate battle
+        </button>
       </div>
+      <div className='col-sm-12' style={styles.space}>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-danger'>Reselect players</button>
+        </Link>
+      </div>
+    </div>
+  </div>
 }
 
 ConfirmBattle.PropTypes = {
