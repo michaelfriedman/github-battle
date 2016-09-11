9a8643052dc045705dcdb6b973f519f0fbf2df6b
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-11 15:47:27 -0700

    Implemented use of ES6 template strings, default params and concise objects.

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -6,22 +6,22 @@ import UserDetails from './UserDetails'
 import MainContainer from './MainContainer'
 import Loading from './Loading'
 
-function ConfirmBattle (props) {
-  return props.isLoading === true
+function ConfirmBattle ({isLoading, playersInfo, onInitiateBattle}) {
+  return isLoading === true
     ? <Loading speed={800} text='Waiting' />
     : <MainContainer>
       <h1>Confirm Players</h1>
       <div className='col-sm-8 col-sm-offset-2'>
         <UserDetailsWrapper header='Player One'>
-          <UserDetails info={props.playersInfo[0]} />
+          <UserDetails info={playersInfo[0]} />
         </UserDetailsWrapper>
         <UserDetailsWrapper header='Player Two'>
-          <UserDetails info={props.playersInfo[1]} />
+          <UserDetails info={playersInfo[1]} />
         </UserDetailsWrapper>
       </div>
       <div className='col-sm-8 col-sm-offset-2'>
         <div className='col-sm-12' style={space}>
-          <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
+          <button type='button' className='btn btn-lg btn-success' onClick={onInitiateBattle}>
             Initiate battle
           </button>
         </div>
